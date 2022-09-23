539d91316198ea8ac39fa4a25a90fe78774251b1
Boram Kim <boram2445@gmail.com>
2022-09-23 20:26:36 +0900

    :sparkles: Feat: 페이지 전환시 스크롤 되는 기능 추가

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,20 @@ import { Provider } from "react-redux";
 import store from "./store";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { fetchGetProducts } from "./reducers/productSlice";
+import ScrollToTop from "./components/common/ScrollToTop";
+
+//앱이 시작되면 상품들 불러오기
+store.dispatch(fetchGetProducts());
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <ScrollToTop />
+      <App />
+    </BrowserRouter>
+  </Provider>
+  // </React.StrictMode>
 );
