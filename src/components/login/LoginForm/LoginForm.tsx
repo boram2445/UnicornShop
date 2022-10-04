b7d698db7571e36b89bef83be7562b00b544f1df
Boram Kim <boram2445@gmail.com>
2022-10-04 16:07:25 +0900

    :lipstick: Design: 로그인 폼 컴포넌트 UI 완성

--- src/components/login/LoginForm/LoginForm.tsx
+++ src/components/login/LoginForm/LoginForm.tsx
@@ -1,23 +1,20 @@
-import React from 'react';
-import { NormalBtn } from '../../common/Button/Button';
-import * as S from './loginFormStyle';
+import React from "react";
+import { NormalBtn } from "../../common/Button/Button";
+import ToggleBtn from "../../common/ToggleBtn/ToggleBtn";
+import * as S from "./loginFormStyle";
 
 function LoginForm() {
-    return (
-        <S.LoginSection>
-            <S.ToggleBox>
-                <S.ToggleBtn>구매회원 로그인</S.ToggleBtn>
-                <S.ToggleBtn disabled={true}>판매회원 로그인</S.ToggleBtn>
-            </S.ToggleBox>
-            <S.LoginForm>
-                <S.LoginInput type='text' placeholder='아이디' />
-                <S.LoginInput type='text' placeholder='비밀번호' />
-                <S.ErrorText>아이디 또는 비밀번호가 일치하지 않습니다.</S.ErrorText>
-                <NormalBtn>로그인</NormalBtn>
-            </S.LoginForm>
-        </S.LoginSection>
-
-    )
+  return (
+    <S.LoginSection>
+      <ToggleBtn />
+      <S.LoginForm>
+        <S.LoginInput type="text" placeholder="아이디" />
+        <S.LoginInput type="text" placeholder="비밀번호" />
+        <S.ErrorText>아이디 또는 비밀번호가 일치하지 않습니다.</S.ErrorText>
+        <NormalBtn>로그인</NormalBtn>
+      </S.LoginForm>
+    </S.LoginSection>
+  );
 }
 
 export default LoginForm;
