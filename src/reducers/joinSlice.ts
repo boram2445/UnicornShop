2c2d5af04fccbb3512337d467a09bbe8e174ca6f
Boram Kim <boram2445@gmail.com>
2022-10-10 19:55:01 +0900

    :sparkles: Feat: 회원가입 아이디 중복확인 api 연결

---
+++ src/reducers/joinSlice.ts
@@ -0,0 +1,61 @@
+import { RootState } from "../store";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const BASE_URL = "https://openmarket.weniv.co.kr";
+
+interface JoinSliceProps {
+  status: string;
+  error: string;
+  joinValue: {
+    username: string;
+    password: string;
+    confirmPassword: string;
+    name: string;
+    phone: string;
+    email: string;
+  };
+}
+
+const initialState: JoinSliceProps = {
+  status: "idle",
+  error: "",
+  joinValue: {
+    username: "",
+    password: "",
+    confirmPassword: "",
+    name: "",
+    phone: "",
+    email: "",
+  },
+};
+
+export const fetchPostUserName = createAsyncThunk(
+  "join/fetchPostUserName",
+  async (username: string) => {
+    const data = { username };
+    const result = await axios.post(`${BASE_URL}/accounts/signup/valid/username/`, data);
+    return result.data;
+  }
+);
+
+export const joinSlice = createSlice({
+  name: "join",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchPostUserName.fulfilled, (state) => {
+      state.status = "succeeded";
+      state.error = "사용 가능한 아이디 입니다 :)";
+    });
+    builder.addCase(fetchPostUserName.rejected, (state, action) => {
+      state.status = "failed";
+      state.error = action.error.message?.includes("400")
+        ? "이미 사용중인 아이디 입니다 :<"
+        : "알 수 없는 에러";
+    });
+  },
+});
+
+export const getUserNameError = (state: RootState) => state.join.error;
+export default joinSlice.reducer;
