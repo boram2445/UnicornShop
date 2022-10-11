695c6d4d38329feaa4e62e34aea74b0c9b810fad
Boram Kim <boram2445@gmail.com>
2022-10-11 21:35:53 +0900

    :art: Refactor: select 박스 컴포넌트 재사용성 높이기 위해 개선

--- src/components/common/SelectInput/SelectInput.tsx
+++ src/components/common/SelectInput/SelectInput.tsx
@@ -3,14 +3,19 @@ import ArrowDownIcon from "../../../assets/icons/icon-down-arrow.svg";
 import ArrowUpIcon from "../../../assets/icons/icon-up-arrow.svg";
 import * as S from "./selectInputStyle";
 
-function DropDown() {
+type SelectInputProps = {
+  selectItems: string[];
+};
+
+function SelectInput({ selectItems }: SelectInputProps) {
   const [onToggle, setOnToggle] = useState(false);
-  const [selected, setSelected] = useState("010");
+  const [selected, setSelected] = useState(selectItems[0]);
 
   const handleItemClick = (item: string) => {
     setSelected(item);
     setOnToggle(!onToggle);
   };
+
   return (
     <S.SelectArticle>
       <S.SelectedBtn
@@ -21,39 +26,16 @@ function DropDown() {
         {selected}
       </S.SelectedBtn>
       <S.SelectList on={onToggle.toString()}>
-        <li>
-          <button type="button" onClick={() => handleItemClick("010")}>
-            010
-          </button>
-        </li>
-        <li>
-          <button type="button" onClick={() => handleItemClick("011")}>
-            011
-          </button>
-        </li>
-        <li>
-          <button type="button" onClick={() => handleItemClick("016")}>
-            016
-          </button>
-        </li>
-        <li>
-          <button type="button" onClick={() => handleItemClick("017")}>
-            017
-          </button>
-        </li>
-        <li>
-          <button type="button" onClick={() => handleItemClick("018")}>
-            018
-          </button>
-        </li>
-        <li>
-          <button type="button" onClick={() => handleItemClick("019")}>
-            019
-          </button>
-        </li>
+        {selectItems.map((item, index) => (
+          <li key={index}>
+            <button type="button" onClick={() => handleItemClick(item)}>
+              {item}
+            </button>
+          </li>
+        ))}
       </S.SelectList>
     </S.SelectArticle>
   );
 }
 
-export default DropDown;
+export default SelectInput;
