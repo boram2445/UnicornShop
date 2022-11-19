3b001dc91c50391317c2a45290ae08d78a0b0b6e
Boram Kim <boram2445@gmail.com>
2022-11-19 15:29:52 +0900

    :sparkles: Feat: 다음  우편번호 찾기 모달로 변경

--- src/components/payment/DeliveryInfo/DeliveryInfo.tsx
+++ src/components/payment/DeliveryInfo/DeliveryInfo.tsx
@@ -1,82 +1,193 @@
-import React from "react";
+import React, { useState } from "react";
+import { closeModal, openModal, selectOpenState } from "../../../features/modalSlice";
+import { useAppDispatch, useAppSelector } from "../../../hooks";
 import { NormalBtn } from "../../common/Button/Button";
+import FormInput from "../FormInput/FormInput";
+import PostAddress from "../PostAddress/PostAddress";
 import * as S from "./deliveryInfoStyle";
 
 function DeliveryInfo() {
+  const dispatch = useAppDispatch();
+
+  //우편 번호 찾기 모달
+  const modal = useAppSelector(selectOpenState);
+
+  //주문자 정보 - 이름, phone 넘버는 미리 받아오기
+  const [ordererInfo, setOrdererInfo] = useState({
+    name: "",
+    phone: "",
+    email: "",
+  });
+  //배송지 정보
+  const [receiverInfo, setReceiverInfo] = useState({
+    name: "",
+    phone1: "",
+    phone2: "",
+    phone3: "",
+    zoneCode: "",
+    address: "",
+    addressDetail: "",
+    message: "",
+  });
+
+  const onOrdererChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setOrdererInfo({ ...ordererInfo, [e.target.name]: e.target.value });
+    console.log(ordererInfo);
+  };
+
+  //우편번호, 주소 팝업창에서 받아오기
+  const getAddress = (zoneCode: string, address: string) => {
+    setReceiverInfo({ ...receiverInfo, zoneCode, address });
+    dispatch(closeModal());
+  };
+
+  // type ObjType = {
+  //   [index: string]: number | string | boolean | null;
+  //   id: number;
+  //   name: string;
+  // };
+
+  // const ordererInput: ObjType[] = [
+  //   {
+  //     id: 1,
+  //     name: "name",
+  //     type: "text",
+  //     label: "이름",
+  //     pattern: "/^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]+$/",
+  //     required: true,
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "phone",
+  //     type: "text",
+  //     label: "휴대폰",
+  //     pattern: "^[0-9]+$",
+  //     required: true,
+  //   },
+  //   {
+  //     id: 3,
+  //     name: "phone",
+  //     type: "email",
+  //     label: "이메일",
+  //     pattern: "^[0-9]+$",
+  //     required: true,
+  //   },
+  // ];
+
   return (
     <section>
       <S.Title>배송정보</S.Title>
-      <form>
-        <table>
-          <S.Catption>주문자 정보</S.Catption>
-          <tbody>
-            <S.Row>
-              <S.LabelText>이름</S.LabelText>
-              <td>
-                <S.Input type="text" />
-              </td>
-            </S.Row>
-            <S.Row>
-              <S.LabelText>휴대폰</S.LabelText>
-              <td>
-                <S.Input type="text" size={3} width="80px" />
-                &nbsp; - &nbsp;
-                <S.Input type="text" size={4} width="100px" />
-                &nbsp; - &nbsp;
-                <S.Input type="text" size={4} width="100px" />
-              </td>
-            </S.Row>
-            <S.Row>
-              <S.LabelText>이메일</S.LabelText>
-              <td>
-                <S.Input type="text" />
-              </td>
-            </S.Row>
-          </tbody>
-        </table>
-        <table>
-          <S.Catption>배송지 정보</S.Catption>
-          <tbody>
-            <S.Row>
-              <S.LabelText>수령인</S.LabelText>
-              <td>
-                <S.Input type="text" />
-              </td>
-            </S.Row>
-            <S.Row>
-              <S.LabelText>휴대폰</S.LabelText>
-              <td>
-                <S.Input type="text" size={3} width="80px" />
-                &nbsp; - &nbsp;
-                <S.Input type="text" size={4} width="100px" />
-                &nbsp; - &nbsp;
-                <S.Input type="text" size={4} width="100px" />
-              </td>
-            </S.Row>
-            <S.Row>
-              <S.LabelText>배송주소</S.LabelText>
-              <td rowSpan={3}>
-                <S.Input type="text" size={5} />
-                <S.BtnWrapper>
-                  <NormalBtn size="small" disabled={true}>
-                    우편번호 조회
-                  </NormalBtn>
-                </S.BtnWrapper>
-                <br />
-                <S.Input type="text" width="800px" />
-                <br />
-                <S.Input type="text" width="800px" />
-              </td>
-            </S.Row>
-            <S.Row>
-              <S.LabelText>배송 메세지</S.LabelText>
-              <td>
-                <S.Input type="text" width="800px" />
-              </td>
-            </S.Row>
-          </tbody>
-        </table>
-      </form>
+      <table>
+        <S.Catption>주문자 정보</S.Catption>
+        <tbody>
+          {/* {ordererInput.map((info) => (
+            <FormInput
+              key={info.id}
+              {...info}
+              value={ordererInfo[info.name]}
+              onChange={onOrdererChange}
+            />
+          ))} */}
+          <S.Row>
+            <S.LabelText>이름</S.LabelText>
+            <td>
+              <S.Input
+                type="text"
+                name="name"
+                onChange={onOrdererChange}
+                pattern="/^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]+$/"
+                required
+              />
+            </td>
+          </S.Row>
+          <S.Row>
+            <S.LabelText>휴대폰</S.LabelText>
+            <td>
+              <S.Input
+                type="text"
+                size={3}
+                width="80px"
+                name="phone1"
+                onChange={onOrdererChange}
+                pattern="^[0-9]+$"
+              />
+              &nbsp; - &nbsp;
+              <S.Input
+                type="text"
+                size={4}
+                width="100px"
+                name="phone2"
+                onChange={onOrdererChange}
+                pattern="^[0-9]+$"
+              />
+              &nbsp; - &nbsp;
+              <S.Input
+                type="text"
+                size={4}
+                width="100px"
+                name="phone3"
+                onChange={onOrdererChange}
+                pattern="^[0-9]+$"
+              />
+            </td>
+          </S.Row>
+          <S.Row>
+            <S.LabelText>이메일</S.LabelText>
+            <td>
+              <S.Input type="email" name="email" onChange={onOrdererChange} />
+            </td>
+          </S.Row>
+        </tbody>
+      </table>
+      <table>
+        <S.Catption>배송지 정보</S.Catption>
+        <tbody>
+          <S.Row>
+            <S.LabelText>수령인</S.LabelText>
+            <td>
+              <S.Input type="text" />
+            </td>
+          </S.Row>
+          <S.Row>
+            <S.LabelText>휴대폰</S.LabelText>
+            <td>
+              <S.Input type="text" size={3} width="80px" />
+              &nbsp; - &nbsp;
+              <S.Input type="text" size={4} width="100px" />
+              &nbsp; - &nbsp;
+              <S.Input type="text" size={4} width="100px" />
+            </td>
+          </S.Row>
+          <S.Row>
+            <S.LabelText>배송주소</S.LabelText>
+            <td rowSpan={3}>
+              <S.Input
+                type="text"
+                width="150px"
+                size={5}
+                placeholder="우편번호"
+                value={receiverInfo.zoneCode}
+              />
+              <S.BtnWrapper>
+                <NormalBtn size="small" onClick={() => dispatch(openModal(""))} type="button">
+                  우편번호 찾기
+                </NormalBtn>
+              </S.BtnWrapper>
+              {modal && <PostAddress getAddress={getAddress} />}
+              <br />
+              <S.Input type="text" width="600px" placeholder="주소" value={receiverInfo.address} />
+              <br />
+              <S.Input type="text" width="400px" placeholder="상세주소" />
+            </td>
+          </S.Row>
+          <S.Row>
+            <S.LabelText>배송 메세지</S.LabelText>
+            <td>
+              <S.Input type="text" width="600px" />
+            </td>
+          </S.Row>
+        </tbody>
+      </table>
     </section>
   );
 }
--- src/components/payment/PostAddress/PostAddress.tsx
+++ src/components/payment/PostAddress/PostAddress.tsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
 import DaumPostcode from "react-daum-postcode";
+import * as S from "./postAddressStyle";
+import deleteIcon from "../../../assets/icons/icon-delete.svg";
+import { useAppDispatch } from "../../../hooks";
+import { closeModal } from "../../../features/modalSlice";
 
 type PostAddressProps = {
   getAddress: (zoneCode: string, address: string) => void;
 };
 
 function PostAddress({ getAddress }: PostAddressProps) {
+  const dispatch = useAppDispatch();
+  const background = useRef() as React.MutableRefObject<HTMLInputElement>;
+
+  //모달창 떴을때 스크롤 방지 - 모달 컴포넌트와 중복
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "unset";
+    };
+  }, []);
+
+  //배경화면 클릭시 모달창 닫기
+  const onBackgroundClick = (e: React.MouseEvent<HTMLElement>) => {
+    if (background.current === e.target) {
+      dispatch(closeModal());
+    }
+  };
   const onCompletePost = (data: any) => {
     let fullAddr = data.address;
     let extraAddr = "";
@@ -25,15 +47,21 @@ function PostAddress({ getAddress }: PostAddressProps) {
   };
 
   const customStyles = {
-    left: "0",
-    margin: "auto",
-    width: "500px",
-    height: "600px",
-    padding: "0",
-    overflow: "hidden",
+    width: "100%",
+    height: "100%",
   };
 
-  return <DaumPostcode autoClose onComplete={onCompletePost} style={customStyles} />;
+  return ReactDOM.createPortal(
+    <S.ModalBackGround ref={background} onClick={onBackgroundClick}>
+      <S.ModalWrapper>
+        <S.CloseBtn onClick={() => dispatch(closeModal())}>
+          <img src={deleteIcon} />
+        </S.CloseBtn>
+        <DaumPostcode autoClose onComplete={onCompletePost} style={customStyles} />
+      </S.ModalWrapper>
+    </S.ModalBackGround>,
+    document.getElementById("portal")!
+  );
 }
 
 export default PostAddress;
