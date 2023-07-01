41c1a34b3657c0b72bbd94a518046a5397e4c5cb
Boram Kim <boram2445@gmail.com>
2023-07-01 16:41:49 +0900

    Fix: 검색창 삭제 버튼 추가, nav 버튼 디자인 수정

--- src/components/common/Header/headerStyle.ts
+++ src/components/common/Header/headerStyle.ts
@@ -1,21 +1,21 @@
 import styled from "styled-components";
+import DeleteIcon from "../../../assets/icons/icon-delete.svg";
 
 const HeaderContainer = styled.header`
-  box-shadow: 0px 4px 5px -4px #0000001a;
+  box-shadow: var(--shadow-light);
 `;
 
 const HeaderContents = styled.div`
   margin: 0 auto;
   max-width: 1380px;
-  padding: 22px;
+  padding: 2rem 3rem;
   display: flex;
   justify-content: space-between;
 `;
 
 const LeftWrap = styled.div`
   display: flex;
-  align-items: center;
-  position: relative;
+  gap: 3rem;
 `;
 
 const Logo = styled.h1<{ logoUrl: string }>`
@@ -26,57 +26,90 @@ const Logo = styled.h1<{ logoUrl: string }>`
   cursor: pointer;
 `;
 
-const Input = styled.input`
-  width: 400px;
-  margin-left: 30px;
-  padding: 13px 0 13px 22px;
-  border: #fa897b 2px solid;
-  border-radius: 50px;
+const SearchForm = styled.form`
+  position: relative;
+`;
+
+const Input = styled.input<{ text?: string; icon?: string }>`
+  position: relative;
+  width: 45rem;
+  padding: 1.2rem 8rem 1.2rem 2.2rem;
+  border: 2px solid var(--color-main);
+  border-radius: 5rem;
   font-size: 1.6rem;
+
+  //삭제 버튼 커스텀
+  &[type="search"]::-webkit-search-cancel-button {
+    -webkit-appearance: none;
+    position: absolute;
+    right: -1.5rem;
+    padding: 0 11rem 0 2rem;
+    width: 2rem;
+    height: 2rem;
+    background: url(${DeleteIcon}) center center no-repeat;
+    cursor: pointer;
+  }
 `;
 
 const InputBtn = styled.button<{ icon: string }>`
-  padding: 0;
-  width: 28px;
-  height: 28px;
   position: absolute;
   right: 22px;
+  top: calc(50% - 15px);
+  width: 2.8rem;
+  height: 2.8rem;
+  padding: 0;
   background: url(${({ icon }) => icon}) no-repeat center;
 `;
 
 const RightWrap = styled.div`
   display: flex;
-  gap: 26px;
+  align-items: center;
+  gap: 1rem;
 `;
 
-const NavButton = styled.button<{ color?: string }>`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 4px;
+const NavBtn = styled.button<{ color?: string }>`
   position: relative;
-  max-width: 60px;
+  width: 4.5rem;
+  height: 4.5rem;
+  border-radius: 50%;
   cursor: pointer;
-  font-size: 1.2rem;
   span {
     color: ${({ color }) => color};
   }
+  svg {
+    width: 2.5rem;
+    height: 2.5rem;
+  }
+  &:hover {
+    background-color: var(--color-brightGrey);
+  }
 `;
 
-const MyPageWrap = styled.div`
+const UserBtn = styled(NavBtn)`
+  width: 12rem;
+  padding: 0 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 2rem;
+  font-size: 1.2rem;
+  svg {
+    margin-right: 1rem;
+  }
+`;
+
+const UerModalWrap = styled.div`
   position: relative;
 `;
 
 // Center Header
 const CenterContents = styled(HeaderContents)`
-  max-width: 1380px;
   align-items: center;
   justify-content: start;
 `;
 
 const SmallLogo = styled(Logo)`
-  width: 80px;
+  width: 8rem;
   height: 24px;
 `;
 
@@ -93,11 +126,13 @@ export {
   LeftWrap,
   RightWrap,
   Logo,
+  SearchForm,
   Input,
   InputBtn,
-  NavButton,
+  NavBtn,
+  UserBtn,
   SmallLogo,
   TitleText,
   CenterContents,
-  MyPageWrap,
+  UerModalWrap,
 };
