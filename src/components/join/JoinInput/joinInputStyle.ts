9f53a2dd0061e01e02f588e56caa450243ac3e3f
Boram Kim <boram2445@gmail.com>
2023-07-08 15:00:15 +0900

    Style: px을 rem으로 수정

--- src/components/join/JoinInput/joinInputStyle.ts
+++ src/components/join/JoinInput/joinInputStyle.ts
@@ -2,12 +2,12 @@ import styled from "styled-components";
 
 const InputBox = styled.div`
   button {
-    margin-left: 10px;
+    margin-left: 1rem;
   }
 `;
 
 const LabelText = styled.label`
-  margin-bottom: 10px;
+  margin-bottom: 1rem;
   display: block;
   font-size: 1.6rem;
   line-height: 2rem;
@@ -16,10 +16,10 @@ const LabelText = styled.label`
 
 const Input = styled.input<{ width?: string; icon?: string }>`
   width: ${({ width }) => (width ? width : "100%")};
-  padding: 16px;
+  padding: 1.6rem;
   font-size: 1.6rem;
   line-height: 2rem;
-  border-radius: 5px;
+  border-radius: 0.5rem;
   border: 1px solid var(--color-grey);
   outline-color: var(--color-main);
   background: ${({ icon }) => icon && `url(${icon}) no-repeat right 15px center`};
@@ -27,7 +27,7 @@ const Input = styled.input<{ width?: string; icon?: string }>`
 
 const ErrorText = styled.small<{ error?: string }>`
   display: block;
-  margin: 6px 0 0 10px;
+  margin: 0.6rem 0 0 1rem;
   font-size: 1.4rem;
   font-weight: 500;
   color: var(--color-red);
@@ -36,7 +36,7 @@ const ErrorText = styled.small<{ error?: string }>`
 const InputPhoneBox = styled.div`
   div {
     display: flex;
-    gap: 12px;
+    gap: 1.2rem;
   }
 `;
 
@@ -44,7 +44,7 @@ const InputPhone = styled(Input)``;
 
 const InputEmailBox = styled.div`
   span {
-    margin: 0 11px;
+    margin: 0 1.1rem;
     color: var(--color-darkGrey);
     font-weight: 700;
     font-size: 1.6rem;
