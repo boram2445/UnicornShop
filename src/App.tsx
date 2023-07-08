acdbbb821459eb73c94ba4b758714bf0e5058820
Boram Kim <boram2445@gmail.com>
2023-07-08 16:34:43 +0900

    Rename: 파일 위치 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import GlobalStyle from "./assets/globalStyle";
+import GlobalStyle from "./globalStyle";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import HomePage from "./pages/HomePage/HomePage";
--- src/globalStyle.ts
+++ src/globalStyle.ts
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
-import "./fonts/fonts.css";
+import "./assets/fonts/fonts.css";
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
